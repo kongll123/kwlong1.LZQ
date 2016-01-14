fee220131b14df1c49caa217ec33f6b46f173572
lanou3g <243234821@qq.com>
2016-01-14 10:13:04 +0800

    版本1.0

---
+++ GITXcodeGCC/GITXcodeGCC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GITXcodeGCC
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 kwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GITXcodeGCC/GITXcodeGCCTests/GITXcodeGCCTests.m
@@ -0,0 +1,39 @@
+//
+//  GITXcodeGCCTests.m
+//  GITXcodeGCCTests
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 kwl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GITXcodeGCCTests : XCTestCase
+
+@end
+
+@implementation GITXcodeGCCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
